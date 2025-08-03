f72f922c00b84ccbec70cd5d98b5aeea0d0635f8
kris <krisekteles@gmail.com>
2025-08-03 12:02:46 +0300

    movie App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MovieCard from './components/MovieCard';
+
+function App() {
+  return (
+    <div className="App">
+     < MovieCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MovieCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MovieCard = () => {
+  return (
+    <div>
+<h1> movie app</h1>
+ <div className='card'>
+  <img src='' />
+ </div>
+    </div>
+  )
+}
+
+export default MovieCard
