52e25035738652225beabb510abacf554ad61f9b
kris <krisekteles@gmail.com>
2025-08-14 22:14:52 +0300

    input for the url of the movie

--- src/components/AddMovies.js
+++ src/components/AddMovies.js
@@ -1,49 +1,61 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import MovieCard from "./MovieCard";
 
-const AddMovies = ({onAddMovies}) => {
+const AddMovies = ({ onAddMovies }) => {
+  const [newMovie, setNewMovie] = useState({
+    title: "",
+    description: "",
+    posterURL: "",
+    rating: "",
+  });
 
-    const [newMovie, setNewMovie] = useState({
-        title:'',
-        description:'',
-        posterURL:'',
-        rating:''
-    });
-
-    const handleInputChange = (e)=>{
-        const {name, value} = e.target;
-        setNewMovie(prev => ({...prev, [name]:value}));
-    };
-    const handleSubmit =(e) =>{
-        e.preventDefault();
-        if(newMovie.title && newMovie.description && newMovie.posterURL && newMovie.rating){
-            onAddMovies({
-                ...newMovie,
-                id:Date.now(),
-                rating:parseFloat(newMovie.rating)
-            });
-            setNewMovie({title:'',description:'',posterURL:'',rating:'',})
-        }
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewMovie((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      newMovie.title &&
+      newMovie.description &&
+      newMovie.posterURL &&
+      newMovie.rating
+    ) {
+      onAddMovies({
+        ...newMovie,
+        id: Date.now(),
+        rating: parseFloat(newMovie.rating),
+      });
+      setNewMovie({ title: "", description: "", posterURL: "", rating: "" });
     }
+  };
   return (
-   <form className='add-movie' onSubmit={handleSubmit}>
-    <h2>Add new Movie</h2>
-    <input 
-    name="title"
-    placeholder='Title'
-    value={newMovie.title}
-    onChange={handleInputChange}
-    required
-    />
+    <form className="add-movie" onSubmit={handleSubmit}>
+      <h2>Add new Movie</h2>
+      <input
+        name="title"
+        placeholder="Title"
+        value={newMovie.title}
+        onChange={handleInputChange}
+        required
+      />
 
-<textarea 
-name='description'
-placeholder='Description'
-value={newMovie.description}
-onChange={handleInputChange}
-required
-/>
-   </form>
-  )
-}
+      <textarea
+        name="description"
+        placeholder="Description"
+        value={newMovie.description}
+        onChange={handleInputChange}
+        required
+      />
+      <input 
+      name="posterURL"
+      placeholder="Poster URL here"
+      value={newMovie.posterURL}
+      onChange={handleInputChange}
+      required
+      />
+    </form>
+  );
+};
 
-export default AddMovies
+export default AddMovies;
