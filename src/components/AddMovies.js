b10dd6f7bacfbd9f2dce00f317aa738d248bd0d6
kris <krisekteles@gmail.com>
2025-08-14 22:04:23 +0300

    new movie set

--- src/components/AddMovies.js
+++ src/components/AddMovies.js
@@ -1,13 +1,29 @@
 import React, {useState} from 'react';
 
-const AddMovies = ({onAddMovie}) => {
+const AddMovies = ({onAddMovies}) => {
 
     const [newMovie, setNewMovie] = useState({
         title:'',
         description:'',
-        postURL:'',
+        posterURL:'',
         rating:''
     });
+
+    const handleInputChange = (e)=>{
+        const {name, value} = e.target;
+        setNewMovie(prev => ({...prev, [name]:value}));
+    };
+    const handleSubmit =(e) =>{
+        e.preventDefault();
+        if(newMovie.title && newMovie.description && newMovie.posterURL && newMovie.rating){
+            onAddMovies({
+                ...newMovie,
+                id:Date.now(),
+                rating:parseFloat(newMovie.rating)
+            });
+            setNewMovie({title:'',description:'',posterURL:'',rating:'',})
+        }
+    }
   return (
     <div>
       
