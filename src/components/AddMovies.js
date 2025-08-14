8d332e0c95f31ecec86283bcaf00dcfd67feeeff
kris <krisekteles@gmail.com>
2025-08-14 21:49:58 +0300

    usestate for new movie

---
+++ src/components/AddMovies.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+const AddMovies = ({onAddMovie}) => {
+
+    const [newMovie, setNewMovie] = useState({
+        title:'',
+        description:'',
+        postURL:'',
+        rating:''
+    });
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddMovies
