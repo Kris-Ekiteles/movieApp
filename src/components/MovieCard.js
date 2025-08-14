2107c4726ba34d93219ba69e3f0a1456aa3dc1b0
kris <krisekteles@gmail.com>
2025-08-14 19:55:29 +0300

    adding hooks for title and description

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react';
+
 
 const MovieCard = () => {
+  //state hooks for title and description
+
+  const [ltitle, setTitle] = useState('Default Title');
+  const [description, setDescription]=useState('Default Description')
   return (
     <div>
 <h1> movie app</h1>
